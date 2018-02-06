0fd193978858b8723b2d49ba6449d76f525dfa11
Kevin Brey <manbearwiz@gmail.com>
2018-02-06 16:23:08 -0600

    feat(core): create clip-path-to directive

--- src/app/lib/core/clip-path-to/clip-path-to.directive.ts
+++ src/app/lib/core/clip-path-to/clip-path-to.directive.ts
@@ -1,10 +1,21 @@
-import { Directive } from '@angular/core';
+import { Directive, Input, ElementRef, Renderer, OnInit, OnChanges, AfterViewInit } from '@angular/core';
 
 @Directive({
   selector: '[ngvzClipPathTo]'
 })
-export class ClipPathToDirective {
+export class ClipPathToDirective implements AfterViewInit {
 
-  constructor() { }
+  @Input('ngvzClipPathTo') ngvzClipPathTo: SVGGraphicsElement;
+
+  constructor(private el: ElementRef, private renderer: Renderer) { }
+
+  ngAfterViewInit() {
+    this.renderer.invokeElementMethod(this.el.nativeElement, 'insertAdjacentHTML', [
+      'beforebegin',
+      `<clipPath id="clip-${this.ngvzClipPathTo.id}"><use href="#${this.ngvzClipPathTo.id}"></use></clipPath>`
+    ]);
+
+    this.renderer.setElementAttribute(this.el.nativeElement, 'clip-path', `url(#clip-${this.ngvzClipPathTo.id})`);
+  }
 
 }
