40c6f9eba26083ae7b2f76eafe44f5d2fca0e6bd
Kevin Brey <manbearwiz@gmail.com>
2018-02-06 16:22:37 -0600

    feat(core): create transform directive

--- src/app/lib/core/transform/transform.directive.ts
+++ src/app/lib/core/transform/transform.directive.ts
@@ -1,10 +1,17 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Renderer, Input, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[ngvzTransform]'
 })
-export class TransformDirective {
+export class TransformDirective implements OnInit {
 
-  constructor() { }
+  @Input() translateX: string;
+  @Input() translateY: string;
+
+  constructor(private el: ElementRef, private renderer: Renderer) { }
+
+  ngOnInit() {
+    this.renderer.setElementAttribute(this.el.nativeElement, 'transform', `translate(${this.translateX || 0}, ${this.translateY || 0})`);
+  }
 
 }
