df600986cef54b9c09b7b8fbf5bddaac64910447
Kevin Brey <manbearwiz@gmail.com>
2018-02-06 16:34:32 -0600

    feat(force-directed): fill out force-directed component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+import { RootObject } from './lib/force-directed/force-directed.component';
 
 @Component({
   selector: 'ngvz-root',
@@ -9,6 +10,9 @@ import 'rxjs/add/operator/map';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  links$: Observable<any>;
+  nodes$: Observable<any>;
+  miserable$: Observable<any>;
   flare$: any;
   constructor(private http: HttpClient) { }
 
@@ -19,5 +23,9 @@ export class AppComponent implements OnInit {
           ...n,
           class: n.id ? n.id.slice(n.id.lastIndexOf('.') + 1) : ''
         })));
+
+    this.miserable$ = this.http.get<RootObject>('./assets/miserables.json');
+    this.nodes$ = this.miserable$.map(m => m.nodes);
+    this.links$ = this.miserable$.map(m => m.links);
   }
 }
--- src/app/lib/force-directed/force-directed.component.ts
+++ src/app/lib/force-directed/force-directed.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
+import { SimulationNodeDatum, forceSimulation, forceLink, forceManyBody, forceCenter } from 'd3-force';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'ngvz-force-directed',
   templateUrl: './force-directed.component.html',
   styleUrls: ['./force-directed.component.scss']
 })
-export class ForceDirectedComponent implements OnInit {
+export class ForceDirectedComponent implements OnChanges {
 
-  constructor() { }
+  @Input() nodes: GraphNode[];
+  @Input() links: GraphLink[];
+  @Input() width: number;
+  @Input() height: number;
 
-  ngOnInit() {
+  ngOnChanges() {
+    forceSimulation(this.nodes)
+      .force('link', forceLink(this.links).id((d: GraphNode) => d.id))
+      .force('charge', forceManyBody())
+      .force('center', forceCenter(this.width / 2, this.height / 2));
   }
+}
 
+export interface RootObject {
+  nodes: GraphNode[];
+  links: GraphLink[];
+}
+
+export interface GraphLink {
+  source: string;
+  target: string;
+  value: number;
+}
+
+export interface GraphNode extends SimulationNodeDatum {
+  id: string;
+  group: number;
 }
