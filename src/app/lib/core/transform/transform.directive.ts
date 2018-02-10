8588e1fef0d3f454019a0e1d1ccd4f9ecb822b03
Kevin Brey <kevin.brey@omniresources.com>
2018-02-10 10:04:34 -0600

    feat(core): add rotate and translate options to transform

--- src/app/lib/core/transform/transform.directive.ts
+++ src/app/lib/core/transform/transform.directive.ts
@@ -7,11 +7,24 @@ export class TransformDirective implements OnInit {
 
   @Input() translateX: string;
   @Input() translateY: string;
+  @Input() rotate: string;
+  @Input() translate: number[];
+
 
   constructor(private el: ElementRef, private renderer: Renderer) { }
 
   ngOnInit() {
-    this.renderer.setElementAttribute(this.el.nativeElement, 'transform', `translate(${this.translateX || 0}, ${this.translateY || 0})`);
+    let attributeValue = '';
+
+    if (this.translate) {
+      attributeValue = `translate(${this.translate[0]}, ${this.translate[1]})`;
+    } else if (this.rotate) {
+      attributeValue = `rotate(${this.rotate})`;
+    } else {
+      attributeValue = `translate(${this.translateX || 0}, ${this.translateY || 0})`;
+    }
+
+    this.renderer.setElementAttribute(this.el.nativeElement, 'transform', attributeValue);
   }
 
 }
