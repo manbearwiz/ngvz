601a8445a6aeff169a8bc9fe97adf8e5f824d192
Kevin Brey <kevin.brey@omniresources.com>
2018-03-05 01:40:24 -0600

    refactor(histogram): use new ngvz-axis component

--- src/app/lib/histogram/histogram.module.ts
+++ src/app/lib/histogram/histogram.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HistogramComponent } from './histogram.component';
 import { NgvzCoreModule } from '../core';
+import { SvgComponentsModule } from '../svg-components';
 
 @NgModule({
     imports: [
         CommonModule,
         NgvzCoreModule,
+        SvgComponentsModule,
     ],
     declarations: [HistogramComponent],
     exports: [HistogramComponent]
