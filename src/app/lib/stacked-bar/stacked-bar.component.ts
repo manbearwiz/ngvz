aea578ddfb1cbd23b16df90357a9ac0bbee33694
Kevin Brey <kevin.brey@omniresources.com>
2018-04-01 01:39:59 -0500

    refactor(stacked): remove magic numbers, use axis component

--- src/app/lib/stacked-bar/stacked-bar.component.ts
+++ src/app/lib/stacked-bar/stacked-bar.component.ts
@@ -10,6 +10,8 @@ import { schemeYlOrBr } from 'd3-scale-chromatic';
 })
 export class StackedBarComponent implements OnChanges {
 
+  innerHeight: number;
+  innerWidth: number;
   colors: ReadonlyArray<string>;
   x: ScaleBand<string>;
   y: ScaleLinear<number, number>;
@@ -18,15 +20,16 @@ export class StackedBarComponent implements OnChanges {
   @Input() width: number;
   @Input() height: number;
   @Input() data: StackedBarData[];
+  @Input() margin = { top: 20, right: 20, bottom: 30, left: 40 };
+  @Input() barPadding = 0.05;
 
   ngOnChanges() {
     if (!this.width || !this.height || !this.data) {
       return;
     }
 
-    const margin = { top: 20, right: 20, bottom: 30, left: 40 },
-      width = this.width - margin.left - margin.right,
-      height = this.height - margin.top - margin.bottom;
+    this.innerWidth = this.width - this.margin.left - this.margin.right;
+    this.innerHeight = this.height - this.margin.top - this.margin.bottom;
 
     this.model = this.data.map(d => ({
       ...d,
@@ -36,13 +39,13 @@ export class StackedBarComponent implements OnChanges {
     })).sort((a, b) => b.total - a.total);
 
     this.x = scaleBand()
-      .rangeRound([0, width])
-      .paddingInner(0.05)
+      .rangeRound([0, this.innerWidth])
+      .paddingInner(this.barPadding)
       .align(0.1)
       .domain(this.model.map(m => m.name));
 
     this.y = scaleLinear()
-      .rangeRound([height, 0])
+      .rangeRound([this.innerHeight, 0])
       .domain([0, this.model[0].total])
       .nice();
 
