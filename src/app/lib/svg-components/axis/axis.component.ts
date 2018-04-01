7e996b44406ad4058857ee8f88d2cfe36b9a1dd7
Kevin Brey <kevin.brey@omniresources.com>
2018-04-01 01:33:44 -0500

    feat(axis): all defining tick content outside component

--- src/app/lib/svg-components/axis/axis.component.ts
+++ src/app/lib/svg-components/axis/axis.component.ts
@@ -1,17 +1,32 @@
-import { Component, OnInit, Input, OnChanges } from '@angular/core';
-import { ScaleLinear } from 'd3-scale';
+import { Component, OnInit, Input, OnChanges, Directive, QueryList, ContentChildren, ContentChild, AfterContentInit, ViewChild, TemplateRef } from '@angular/core';
+import { ScaleLinear, ScaleBand, scaleLinear } from 'd3-scale';
+
+function isScaleLinear(scale: ScaleLinear<number, number> | ScaleBand<string>): scale is ScaleLinear<number, number> {
+  return (<ScaleLinear<number, number>>scale).ticks !== undefined;
+}
+
+@Directive({
+  selector: '[ngvzTick]',
+})
+export class NgvzTickDirective {
+  constructor(public template: TemplateRef<any>) { }
+}
 
 @Component({
-  selector: 'ngvz-axis,[ngvz-axis]',
+  selector: '[ngvz-axis]',
   templateUrl: './axis.component.html',
   styleUrls: ['./axis.component.scss']
 })
 export class AxisComponent implements OnChanges {
-  @Input() scale: ScaleLinear<number, number>;
+  ticks: string[] | number[];
+  @Input() scale: ScaleLinear<number, number> | ScaleBand<string>;
   @Input() position: AxisPosition;
   @Input() tickSizeOuter = 6;
   @Input() tickSizeInner = 6;
   @Input() tickPadding = 3;
+  @Input() tickOffset = 0;
+
+  @ContentChild(NgvzTickDirective) tickDirective: NgvzTickDirective;
 
   domainPath: string;
   spacing: number;
@@ -29,6 +44,8 @@ export class AxisComponent implements OnChanges {
     this.domainPath = this.vertical
       ? 'M' + this.k * this.tickSizeOuter + ',' + range0 + 'H0.5V' + range1 + 'H' + this.k * this.tickSizeOuter
       : 'M' + range0 + ',' + this.k * this.tickSizeOuter + 'V0.5H' + range1 + 'V' + this.k * this.tickSizeOuter;
+
+    this.ticks = isScaleLinear(this.scale) ? this.scale.ticks() : this.scale.domain();
   }
 }
 
