81a9e475627940385e6a2d57368dc8c5f7fd688b
Kevin Brey <kevin.brey@omniresources.com>
2018-04-01 01:37:55 -0500

    refactor(transform): use HostBinding instead of renderer

--- src/app/lib/core/transform/transform.directive.ts
+++ src/app/lib/core/transform/transform.directive.ts
@@ -1,30 +1,22 @@
-import { Directive, ElementRef, Renderer, Input, OnInit } from '@angular/core';
+import { Directive, Input, HostBinding, OnChanges } from '@angular/core';
 
 @Directive({
   selector: '[ngvzTransform]'
 })
-export class TransformDirective implements OnInit {
-
+export class TransformDirective implements OnChanges {
   @Input() translateX: string;
   @Input() translateY: string;
   @Input() rotate: string;
   @Input() translate: number[];
+  @HostBinding('attr.transform') transform: string;
 
-
-  constructor(private el: ElementRef, private renderer: Renderer) { }
-
-  ngOnInit() {
-    let attributeValue = '';
-
+  ngOnChanges() {
     if (this.translate) {
-      attributeValue = `translate(${this.translate[0]}, ${this.translate[1]})`;
+      this.transform = `translate(${this.translate[0]}, ${this.translate[1]})`;
     } else if (this.rotate) {
-      attributeValue = `rotate(${this.rotate})`;
+      this.transform = `rotate(${this.rotate})`;
     } else {
-      attributeValue = `translate(${this.translateX || 0}, ${this.translateY || 0})`;
+      this.transform = `translate(${this.translateX || 0}, ${this.translateY || 0})`;
     }
-
-    this.renderer.setElementAttribute(this.el.nativeElement, 'transform', attributeValue);
   }
-
 }
